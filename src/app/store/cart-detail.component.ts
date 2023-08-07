048ab2a37d3e5ac5b4c89b29d60b142deb3c2d68
guymn <gjp2267@gmail.com>
2023-08-07 11:59:28 +0700

    template cart detail

--- src/app/store/cart-detail.component.ts
+++ src/app/store/cart-detail.component.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
 
 @Component({
-  template: `
-    <div>
-      <h3 class="bg-info p-1 text-white">Cart Detail</h3>
-    </div>
-  `,
+  templateUrl: 'cart-detail.component.html'
 })
 export class CartDetailComponent {}
