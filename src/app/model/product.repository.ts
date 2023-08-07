5802c7cfd35349eac75028989c0d851dfa6d436e
guymn <gjp2267@gmail.com>
2023-08-07 10:24:37 +0700

    sort page

--- src/app/model/product.repository.ts
+++ src/app/model/product.repository.ts
@@ -8,7 +8,7 @@ export class ProductRepository {
   private categories: string[] = [];
 
   constructor(private dataSource: StaticDataSource) {
-    dataSource.getProducts().subscribe((data) => {
+    this.dataSource.getProducts().subscribe((data) => {
       this.products = data;
       this.categories = data
         .map((p) => p.category ?? '(None)')
