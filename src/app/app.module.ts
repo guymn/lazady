7fb18eca9198fe9ccc1b4fdad66a89b3c58e146f
guymn <gjp2267@gmail.com>
2023-08-07 11:38:01 +0700

    cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { StoreModule } from "./store/store.module";
+import { StoreModule } from './store/store.module';
+import { RouterModule } from '@angular/router';
+import { StoreComponent } from './store/store.component';
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    providers: [],
-    bootstrap: [AppComponent],
-    imports: [
-        BrowserModule,
-        StoreModule
-    ]
+  declarations: [AppComponent],
+  providers: [],
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    StoreModule,
+    RouterModule.forRoot([
+      { path: 'store', component: StoreComponent },
+      { path: 'store', component: StoreComponent },
+      { path: 'store', component: StoreComponent },
+      { path: 'store', component: StoreComponent },
+    ]),
+  ],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { ModelModule } from '../model/model.module';
 import { StoreComponent } from './store.component';
+import { CartSummaryComponent } from './cart-summary.component';
+import { CartDetailComponent } from './cart-detail.component';
+import { CheckoutComponent } from './checkout.component';
 
 @NgModule({
   imports: [ModelModule, BrowserModule, FormsModule],
-  declarations: [StoreComponent],
-  exports: [StoreComponent],
+  declarations: [
+    StoreComponent,
+    CartSummaryComponent,
+    CartDetailComponent,
+    CheckoutComponent,
+  ],
+  exports: [StoreComponent, CartDetailComponent, CheckoutComponent],
 })
 export class StoreModule {}
