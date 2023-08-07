72d44b98bc6330a9d3ce67dc163f80d47cc749ac
guymn <gjp2267@gmail.com>
2023-08-07 11:48:15 +0700

    router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { StoreModule } from './store/store.module';
 import { RouterModule } from '@angular/router';
+import { CartDetailComponent } from './store/cart-detail.component';
+import { CheckoutComponent } from './store/checkout.component';
 import { StoreComponent } from './store/store.component';
 
 @NgModule({
   declarations: [AppComponent],
-  providers: [],
-  bootstrap: [AppComponent],
   imports: [
     BrowserModule,
     StoreModule,
     RouterModule.forRoot([
       { path: 'store', component: StoreComponent },
-      { path: 'store', component: StoreComponent },
-      { path: 'store', component: StoreComponent },
-      { path: 'store', component: StoreComponent },
+      { path: 'cart', component: CartDetailComponent },
+      { path: 'checkout', component: CheckoutComponent },
+      { path: '**', redirectTo: '/store' },
     ]),
   ],
+  providers: [],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
